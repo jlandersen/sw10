b1d06e93bc465a912e93477e1d95b43118bc5b81
Todberg <mtodbe08@student.aau.dk>
2013-03-27 18:23:15 +0100

    .

---
+++ Code/Animus/web/graph.js
@@ -0,0 +1,166 @@
+
+$(function() {
+
+  var clicks = 0;
+  var timer = null;
+
+  _this = this;
+  _this.clicks = clicks;
+  _this.timer = timer;
+
+  var timeout_callback = function(d) {
+    if(_this.clicks == 1) {
+      toggle(d); 
+      update(d); 
+    } else {
+      
+    }
+    _this.clicks = 0;
+    _this.timer = null;
+  }
+
+  var m = [20, 120, 20, 120],
+    w = 1280 - m[1] - m[3],
+    h = 800 - m[0] - m[2],
+    i = 0,
+    root;
+
+  var tree = d3.layout.tree()
+      .size([h, w]);
+
+  var diagonal = d3.svg.diagonal()
+      .projection(function(d) { return [d.y, d.x]; });
+
+  var vis = d3.select("#graph").append("svg:svg")
+      .attr("width", w + m[1] + m[3])
+      .attr("height", h + m[0] + m[2])
+      .append("svg:g")
+      .attr("transform", "translate(" + m[3] + "," + m[0] + ")");
+
+  json = JSON.parse(dataset)
+  root = json;
+  root.x0 = h / 2;
+  root.y0 = 0;
+
+  function toggleAll(d) {
+    if(d.children) {
+      d.children.forEach(toggleAll);
+      toggle(d);
+    }
+  }
+
+  // Initialize the display to show a few nodes.
+  root.children.forEach(toggleAll);
+  toggle(root);
+
+  update(root);
+
+  function update(source) {
+    var duration = d3.event && d3.event.altKey ? 5000 : 500;
+
+    // Compute the new tree layout.
+    var nodes = tree.nodes(root).reverse();
+
+    // Normalize for fixed-depth.
+    nodes.forEach(function(d) { d.y = d.depth * 180; });
+
+    // Update the nodes…
+    var node = vis.selectAll("g.node")
+        .data(nodes, function(d) { return d.id || (d.id = ++i); });
+
+    // Enter any new nodes at the parent's previous position.
+    var nodeEnter = node.enter().append("svg:g")
+        .attr("class", "node")
+        .attr("transform", function(d) { return "translate(" + source.y0 + "," + source.x0 + ")"; })
+        .on("click", function(d) {
+          if(_this.timer == null) {
+            _this.timer = setTimeout(timeout_callback, 200, d);
+          }
+          _this.clicks++;
+        });
+
+    nodeEnter.append("svg:circle")
+        .attr("r", 10)
+        .style("fill", function(d) { return d._children ? "#3a87ad" : "#fff"; });
+
+    nodeEnter.append("svg:text")
+        .attr("x", function(d) { return d.children || d._children ? -10 : 10; })
+        .attr("dy", "-1.3em")
+        .attr("text-anchor", function(d) { return d.children || d._children ? "end" : "start"; })
+        .text(function(d) { return d.name; })
+        .style("fill-opacity", 1e-6)
+        .style('stroke', function(d) { return d.color });
+
+    // Transition nodes to their new position.
+    var nodeUpdate = node.transition()
+        .duration(duration)
+        .attr("transform", function(d) { return "translate(" + d.y + "," + d.x + ")"; });
+
+    nodeUpdate.select("circle")
+        .attr("r", 10)
+        .style("fill", function(d) { return d._children ? "#3a87ad" : "#fff"; });
+
+    nodeUpdate.select("text")
+        .style("fill-opacity", 1)
+        .style('stroke', function(d) { return d.color });
+
+    // Transition exiting nodes to the parent's new position.
+    var nodeExit = node.exit().transition()
+        .duration(duration)
+        .attr("transform", function(d) { return "translate(" + source.y + "," + source.x + ")"; })
+        .remove();
+
+    nodeExit.select("circle")
+        .attr("r", 10);
+
+    nodeExit.select("text")
+        .style("fill-opacity", 1e-6)
+        .style('stroke', function(d) { return d.color });
+
+    // Update the links…
+    var link = vis.selectAll("path.link")
+        .data(tree.links(nodes), function(d) { return d.target.id; });
+
+    // Enter any new links at the parent's previous position.
+    link.enter().insert("svg:path", "g")
+        .attr("class", "link")
+        .attr("d", function(d) {
+          var o = {x: source.x0, y: source.y0};
+          return diagonal({source: o, target: o});
+        })
+      .transition()
+        .duration(duration)
+        .attr("d", diagonal);
+
+    // Transition links to their new position.
+    link.transition()
+        .duration(duration)
+        .attr("d", diagonal);
+
+    // Transition exiting nodes to the parent's new position.
+    link.exit().transition()
+        .duration(duration)
+        .attr("d", function(d) {
+          var o = {x: source.x, y: source.y};
+          return diagonal({source: o, target: o});
+        })
+        .remove();
+
+    // Stash the old positions for transition.
+    nodes.forEach(function(d) {
+      d.x0 = d.x;
+      d.y0 = d.y;
+    });
+  }
+
+  // Toggle children.
+  function toggle(d) {
+    if (d.children) {
+      d._children = d.children;
+      d.children = null;
+    } else {
+      d.children = d._children;
+      d._children = null;
+    }
+  }  
+})
---
+++ Code/Animus/web/sample.js
@@ -0,0 +1,1 @@
+var dataset = '{"name": "MAT", "color": "green", "link": "tralala", "children": [{"name": "TRI", "color": "green", "link": "tralala", "children": [{"name": "A","color": "green", "link": "tralala", "children": [{"name": "B", "color": "red", "link": "tralala"},{"name": "C", "color": "red", "link": "tralala"}]}]}]}'
