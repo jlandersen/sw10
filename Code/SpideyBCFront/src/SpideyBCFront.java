39439356eae22ebbb438c75e107c0253f967559b
Todberg <mtodbe08@student.aau.dk>
2013-06-13 11:49:07 +0200

    Front-end GUI design made scalable/resizable Added run configuration option

--- Code/SpideyBCFront/src/SpideyBCFront.java
+++ Code/SpideyBCFront/src/SpideyBCFront.java
@@ -15,7 +15,6 @@ import org.eclipse.swt.widgets.Button;
 import org.eclipse.swt.widgets.Combo;
 import org.eclipse.swt.widgets.Composite;
 import org.eclipse.swt.widgets.Display;
-import org.eclipse.swt.widgets.Group;
 import org.eclipse.swt.widgets.Label;
 import org.eclipse.swt.widgets.MessageBox;
 import org.eclipse.swt.widgets.Shell;
@@ -23,23 +22,22 @@ import org.eclipse.swt.widgets.Text;
 import org.eclipse.wb.swt.SWTResourceManager;
 
 public class SpideyBCFront {
-	private static Text sourceText;
-	private static Text jarText;
-	private static Text mainclassText;
-	private static Text modelText;
-	private static Text outputText;
-	private static Text analysisEntryText;
-	private static Text consoleOutputText;
+	
+	/* Input fields */
+	private static Text txtSourceFilesRootDirectory;
+	private static Text txtApplicationJar;
+	private static Text txtMainClass;
+	private static Text txtJVMModel;
+	private static Text txtReportOutputDirectory;
+	private static Text txtEntryMethods;
+	private static Text txtConsoleOutput;
 
 	private static FileBrowser fileBrowser;
 	private static Button stdLibraryCheck;
+	private static Combo cmbRunConfiguration;
+
+	private static Properties properties;
 	
-	private static Properties prop;
-	
-	/**
-	 * Launch the application.
-	 * @param args
-	 */
 	public static void main(String[] args) {
 		Display display = Display.getDefault();
 		Shell shell = new Shell();
@@ -47,35 +45,30 @@ public class SpideyBCFront {
 		fileBrowser = new FileBrowser(display);
 		
 		shell.setImage(SWTResourceManager.getImage(SpideyBCFront.class, "/javax/swing/plaf/metal/icons/ocean/info.png"));
-		shell.setSize(1095, 476);
 		shell.setText("SpideyBC - Static Resource Analysis of Java Bytecode");
-
-		GridLayout gridLayout = new GridLayout();
-		gridLayout.makeColumnsEqualWidth = true;
-		gridLayout.numColumns = 2;
-		gridLayout.verticalSpacing = SWT.FILL;
-		gridLayout.horizontalSpacing = SWT.FILL;
-		shell.setLayout(gridLayout);
-				
-		gridLayout = new GridLayout();
-		gridLayout.numColumns = 2;
-		GridData gridData = new GridData();
-		Composite composite = new Composite(shell, SWT.NULL);
-		composite.setLayout(gridLayout);
+		shell.setSize(850, 500);
 		
-		Label lblApplicationJar = new Label(composite, SWT.NONE);
-		lblApplicationJar.setText("Application JAR:");
-		gridData = new GridData();
+		GridData parentData = new GridData(SWT.FILL, SWT.FILL, true, true);
+		shell.setLayout(new GridLayout(2, true));
+		shell.setLayoutData(parentData);
+		
+		Composite leftContainer = new Composite(shell, SWT.BORDER);
+		leftContainer.setLayout(new GridLayout(2, false));
+		leftContainer.setLayoutData(new GridData(GridData.FILL, GridData.FILL, true, false));
+		
+		Label lblApplicationJar = new Label(leftContainer, SWT.NONE);
+		lblApplicationJar.setText("Application jar:");
+		GridData gridData = new GridData();
 		gridData.horizontalSpan = 2;
 		lblApplicationJar.setLayoutData(gridData);
 		
-		sourceText = new Text(composite, SWT.BORDER);
+		txtSourceFilesRootDirectory = new Text(leftContainer, SWT.BORDER);
 		gridData = new GridData();
 		gridData.grabExcessHorizontalSpace = true;
 		gridData.horizontalAlignment = SWT.FILL;
-		sourceText.setLayoutData(gridData);
+		txtSourceFilesRootDirectory.setLayoutData(gridData);
 		
-		Button jarButton = new Button(composite, SWT.NONE);
+		Button jarButton = new Button(leftContainer, SWT.NONE);
 		gridData = new GridData();
 		gridData.horizontalAlignment = SWT.RIGHT;
 		jarButton.setLayoutData(gridData);
@@ -84,32 +77,39 @@ public class SpideyBCFront {
 			public void widgetSelected(SelectionEvent e) {
 				String file = fileBrowser.openFile();
 				if(file != null)
-					jarText.setText(file);
+					txtApplicationJar.setText(file);
 			}
 		});
 		jarButton.setText("...");
 		
-		stdLibraryCheck = new Button(composite, SWT.CHECK);
+		stdLibraryCheck = new Button(leftContainer, SWT.CHECK);
 		stdLibraryCheck.setText("Includes standard library");
 		gridData = new GridData();
 		gridData.horizontalSpan = 2;
 		gridData.verticalAlignment = SWT.TOP;
 		stdLibraryCheck.setLayoutData(gridData);
 		
-		Label lblSourceFilesRoot = new Label(composite, SWT.NONE);
+		Label label = new Label(leftContainer, SWT.SEPARATOR | SWT.HORIZONTAL);
+		gridData = new GridData();
+		gridData.horizontalSpan = 2;
+		gridData.horizontalAlignment = SWT.FILL;
+		gridData.grabExcessHorizontalSpace = true;
+		label.setLayoutData(gridData);
+		
+		Label lblSourceFilesRoot = new Label(leftContainer, SWT.NONE);
 		gridData = new GridData();
 		gridData.horizontalSpan = 2;
 		lblSourceFilesRoot.setLayoutData(gridData);
 		lblSourceFilesRoot.setText("Source files root directory:");
 		
-		jarText = new Text(composite, SWT.BORDER);
+		txtApplicationJar = new Text(leftContainer, SWT.BORDER);
 		gridData = new GridData();
 		gridData = new GridData();
 		gridData.grabExcessHorizontalSpace = true;
 		gridData.horizontalAlignment = SWT.FILL;
-		jarText.setLayoutData(gridData);
+		txtApplicationJar.setLayoutData(gridData);
 		
-		Button sourceButton = new Button(composite, SWT.NONE);
+		Button sourceButton = new Button(leftContainer, SWT.NONE);
 		gridData = new GridData();
 		gridData.horizontalAlignment = SWT.RIGHT;
 		sourceButton.setLayoutData(gridData);
@@ -118,37 +118,37 @@ public class SpideyBCFront {
 			public void widgetSelected(SelectionEvent e) {
 				String dir = fileBrowser.openDirectory();
 				if(dir != null)
-					sourceText.setText(dir);
+					txtSourceFilesRootDirectory.setText(dir);
 			}
 		});
 		sourceButton.setText("...");
 		
-		Label lblMainClassspecify = new Label(composite, SWT.NONE);
-		lblMainClassspecify.setText("Main class (specify fully qualified type):");
+		Label lblMainClassspecify = new Label(leftContainer, SWT.NONE);
+		lblMainClassspecify.setText("Main class (fully qualified type):");
 		gridData = new GridData();
 		gridData.horizontalSpan = 2;
 		lblMainClassspecify.setLayoutData(gridData);
 		
-		mainclassText = new Text(composite, SWT.BORDER);
+		txtMainClass = new Text(leftContainer, SWT.BORDER);
 		gridData = new GridData();
 		gridData.grabExcessHorizontalSpace = true;
 		gridData.horizontalAlignment = SWT.FILL;
 		gridData.horizontalSpan = 2;
-		mainclassText.setLayoutData(gridData);
+		txtMainClass.setLayoutData(gridData);
 		
-		Label lblJvmModel = new Label(composite, SWT.NONE);
-		lblJvmModel.setText("JVM Model:");
+		Label lblJvmModel = new Label(leftContainer, SWT.NONE);
+		lblJvmModel.setText("JVM model:");
 		gridData = new GridData();
 		gridData.horizontalSpan = 2;
 		lblJvmModel.setLayoutData(gridData);
 		
-		modelText = new Text(composite, SWT.BORDER);
+		txtJVMModel = new Text(leftContainer, SWT.BORDER);
 		gridData = new GridData();
 		gridData.grabExcessHorizontalSpace = true;
 		gridData.horizontalAlignment = SWT.FILL;
-		modelText.setLayoutData(gridData);
+		txtJVMModel.setLayoutData(gridData);
 		
-		Button modelButton = new Button(composite, SWT.NONE);
+		Button modelButton = new Button(leftContainer, SWT.NONE);
 		gridData = new GridData();
 		gridData.horizontalAlignment = SWT.RIGHT;
 		modelButton.setLayoutData(gridData);
@@ -157,24 +157,24 @@ public class SpideyBCFront {
 			public void widgetSelected(SelectionEvent e) {
 				String file = fileBrowser.openFile();
 				if(file != null)
-					modelText.setText(file);
+					txtJVMModel.setText(file);
 			}
 		});
 		modelButton.setText("...");
 		
-		Label lblReportOutputDirectory = new Label(composite, SWT.NONE);
+		Label lblReportOutputDirectory = new Label(leftContainer, SWT.NONE);
 		lblReportOutputDirectory.setText("Report output directory:");
 		gridData = new GridData();
 		gridData.horizontalSpan = 2;
 		lblReportOutputDirectory.setLayoutData(gridData);
 		
-		outputText = new Text(composite, SWT.BORDER);
+		txtReportOutputDirectory = new Text(leftContainer, SWT.BORDER);
 		gridData = new GridData();
 		gridData.horizontalAlignment = SWT.FILL;
 		gridData.grabExcessHorizontalSpace = true;
-		outputText.setLayoutData(gridData);
+		txtReportOutputDirectory.setLayoutData(gridData);
 		
-		Button outputButton = new Button(composite, SWT.NONE);
+		Button outputButton = new Button(leftContainer, SWT.NONE);
 		gridData = new GridData();
 		gridData.horizontalAlignment = SWT.RIGHT;
 		outputButton.setLayoutData(gridData);
@@ -183,28 +183,50 @@ public class SpideyBCFront {
 			public void widgetSelected(SelectionEvent e) {
 				String dir = fileBrowser.openDirectory();
 				if(dir != null)
-					outputText.setText(dir);
+					txtReportOutputDirectory.setText(dir);
 			}
 		});
 		outputButton.setText("...");
 		
-		Label lblAnalysisEntryPoints = new Label(composite, SWT.NONE);
-		lblAnalysisEntryPoints.setText("Analysis entry points:");
+		Label lblEntryMethods = new Label(leftContainer, SWT.NONE);
+		lblEntryMethods.setText("Entry methods (comma separated compiler signatures):");
 		gridData = new GridData();
 		gridData.horizontalSpan = 2;
-		lblAnalysisEntryPoints.setLayoutData(gridData);
+		lblEntryMethods.setLayoutData(gridData);
 		
-		analysisEntryText = new Text(composite, SWT.BORDER);
+		txtEntryMethods = new Text(leftContainer, SWT.BORDER);
 		gridData = new GridData();
 		gridData.grabExcessHorizontalSpace = true;
 		gridData.horizontalAlignment = SWT.FILL;
 		gridData.horizontalSpan = 2;
-		analysisEntryText.setLayoutData(gridData);
+		txtEntryMethods.setLayoutData(gridData);
 		
-		Button startAnalysisButton = new Button(composite, SWT.NONE);
+		Label lblRunConfiguration = new Label(leftContainer, SWT.NONE);
+		lblRunConfiguration.setText("Run configuration:");
+		gridData = new GridData();
+		gridData.horizontalSpan = 2;
+		lblRunConfiguration.setLayoutData(gridData);
+		
+		cmbRunConfiguration = new Combo(leftContainer, SWT.READ_ONLY);
+		cmbRunConfiguration.add("debug");
+		cmbRunConfiguration.add("deploy");
+		cmbRunConfiguration.select(0);
+		gridData = new GridData();
+		gridData.horizontalSpan = 2;
+		gridData.grabExcessHorizontalSpace = true;
+		gridData.horizontalAlignment = SWT.FILL;
+		cmbRunConfiguration.setLayoutData(gridData);
+		
+		Button startAnalysisButton = new Button(leftContainer, SWT.NONE);
 		gridData = new GridData();
 		gridData.horizontalAlignment = SWT.RIGHT;
+		gridData.verticalAlignment = SWT.TOP;
+		gridData.verticalSpan = 2;
 		gridData.horizontalSpan = 2;
+		gridData.grabExcessVerticalSpace = true;
+		gridData.grabExcessHorizontalSpace = true;
+		gridData.minimumHeight = 50;
+		gridData.minimumWidth = 150;
 		startAnalysisButton.setLayoutData(gridData);
 		startAnalysisButton.addSelectionListener(new SelectionAdapter() {
 			@Override
@@ -214,18 +236,25 @@ public class SpideyBCFront {
 		});
 		startAnalysisButton.setText("Start analysis");
 		
+		Composite rightContainer = new Composite(shell, SWT.BORDER);
+	    rightContainer.setLayout(new GridLayout(1, true));
+	    rightContainer.setLayoutData(new GridData(SWT.FILL, SWT.FILL, true, true));
 		
-		Composite rightContainer = new Composite(shell, SWT.NULL);
-		gridLayout = new GridLayout();
-		rightContainer.setLayout(gridLayout);
-		
-		Label lblConsoleOutput = new Label(rightContainer, SWT.NONE);
+	    Label lblConsoleOutput = new Label(rightContainer, SWT.NONE);
 		lblConsoleOutput.setText("Console output:");
 		gridData = new GridData();
+		gridData.grabExcessHorizontalSpace = true;
+		gridData.horizontalAlignment = SWT.FILL;
 		lblConsoleOutput.setLayoutData(gridData);
 		
-		consoleOutputText = new Text(rightContainer, SWT.BORDER | SWT.READ_ONLY | SWT.WRAP | SWT.H_SCROLL | SWT.V_SCROLL | SWT.CANCEL | SWT.MULTI);
-
+		txtConsoleOutput = new Text(rightContainer, SWT.BORDER | SWT.READ_ONLY | SWT.WRAP | SWT.H_SCROLL | SWT.V_SCROLL | SWT.CANCEL | SWT.MULTI);
+		gridData = new GridData();
+		gridData.grabExcessHorizontalSpace = true;
+		gridData.grabExcessVerticalSpace = true;
+		gridData.horizontalAlignment = SWT.FILL;
+		gridData.verticalAlignment = SWT.FILL;
+		txtConsoleOutput.setLayoutData(gridData);
+		
 		readProperties();
 		
 		shell.open();
@@ -238,39 +267,41 @@ public class SpideyBCFront {
 	}
 	
 	private static void readProperties() {
-		prop = new Properties();
+		properties = new Properties();
 		
 		try {
-			prop.load(new FileInputStream("config.properties"));
+			properties.load(new FileInputStream("config.properties"));
 			
-			sourceText.setText(prop.getProperty("source"));
-			jarText.setText(prop.getProperty("application"));
-			mainclassText.setText(prop.getProperty("mainClass"));
-			modelText.setText(prop.getProperty("model"));
-			outputText.setText(prop.getProperty("output"));
-			analysisEntryText.setText(prop.getProperty("entryPoints"));
+			txtApplicationJar.setText(properties.getProperty("application"));
+			txtSourceFilesRootDirectory.setText(properties.getProperty("source"));
+			txtMainClass.setText(properties.getProperty("mainClass"));
+			txtJVMModel.setText(properties.getProperty("model"));
+			txtReportOutputDirectory.setText(properties.getProperty("output"));
+			txtEntryMethods.setText(properties.getProperty("entryPoints"));
+			cmbRunConfiguration.select(getRunConfiguration(properties.getProperty("runConfiguration")));
 			
-			stdLibraryCheck.setSelection(Boolean.parseBoolean(prop.getProperty("jarIncludesSTDLibraries")));
+			stdLibraryCheck.setSelection(Boolean.parseBoolean(properties.getProperty("jarIncludesSTDLibraries")));
 		} catch(IOException e) {
 			e.printStackTrace();
 		}
 	}
 		
 	public static void startAnalysis() {
-		consoleOutputText.setText("");
+		txtConsoleOutput.setText("");
 
 		List<String> args = new ArrayList<String>();
 		ClassLoader cl = SpideyBCFront.class.getClassLoader();
 		URL url = cl.getResource("SpideyBCFront.class");
 		
 		String path = url.getPath().substring(0, url.getPath().lastIndexOf('/') + 1) + "SpideyBC.jar";
-		String jar = jarText.getText();
-		String model = modelText.getText();
+		String jar = txtApplicationJar.getText();
+		String model = txtJVMModel.getText();
 		String jar_includes_std_libraries = Boolean.toString(stdLibraryCheck.getSelection());
-		String source = sourceText.getText();
-		String output = outputText.getText();
-		String mainClass = mainclassText.getText();
-		String entrypoints = analysisEntryText.getText();
+		String source = txtSourceFilesRootDirectory.getText();
+		String output = txtReportOutputDirectory.getText();
+		String mainClass = txtMainClass.getText();
+		String entrypoints = txtEntryMethods.getText();
+		String runConfiguration = getRunConfiguration(cmbRunConfiguration.getSelectionIndex());
 		
 		if(!jar.equals("") && !model.equals("") 
 				&& !jar_includes_std_libraries.equals("") 
@@ -288,6 +319,7 @@ public class SpideyBCFront {
 				prop.setProperty("output", output);
 				prop.setProperty("mainClass", mainClass);
 				prop.setProperty("entryPoints", entrypoints);
+				prop.setProperty("runConfiguration", runConfiguration);
 				
 				prop.store(new FileOutputStream("config.properties"), null);
 			} catch(IOException e) {
@@ -302,28 +334,24 @@ public class SpideyBCFront {
 			args.add("-output_dir " + output);
 			args.add("-main_class " + mainClass);
 			args.add("-entry_points " + entrypoints);
-			/*
-			ProcessBuilder animusProcess = new ProcessBuilder(args);
-			*/
+			args.add("-run_configuration " + runConfiguration);
 			
 			String rootPath = url.getPath().substring(0, url.getPath().lastIndexOf('/') + 1);
-			String cp = String.format("\"%s;%sgson-2.2.2.jar;%sjavailp-1.2a.jar;%slpsolve55j.jar;%svelocity-1.7-dep.jar\"", path, rootPath, rootPath, rootPath, rootPath);
-			
 			Runtime rt = Runtime.getRuntime();
 			Process p = null;
 			try {
 				
-				String runExec = String.format("java -cp %s sw10.spideybc.program.Program -application %s -jvm_model %s -jar_includes_std_libraries %s -source_files_root_dir %s -output_dir %s -main_class %s -entry_points %s", 
-						rootPath + "../libs/*", jar, model, jar_includes_std_libraries, source, output, mainClass, entrypoints);
+				String runExec = String.format("java -cp %s sw10.spideybc.program.Program -application %s -jvm_model %s -jar_includes_std_libraries %s -source_files_root_dir %s -output_dir %s -main_class %s -run_configuration %s -entry_points %s", 
+						rootPath + "../libs/*", jar, model, jar_includes_std_libraries, source, output, mainClass, runConfiguration, entrypoints);
 				
-				consoleOutputText.setText(runExec + "\r\n");
+				txtConsoleOutput.setText(runExec + "\r\n");
 				p = rt.exec(runExec);
 			} catch (IOException e1) {
 				e1.printStackTrace();	
 			};
 			
-			StreamGobbler outputStream = new StreamGobbler(p.getInputStream(), consoleOutputText);
-			StreamGobbler errorStream = new StreamGobbler(p.getErrorStream(), consoleOutputText);
+			StreamGobbler outputStream = new StreamGobbler(p.getInputStream(), txtConsoleOutput);
+			StreamGobbler errorStream = new StreamGobbler(p.getErrorStream(), txtConsoleOutput);
 			outputStream.start();
 			errorStream.start();
 			try {
@@ -337,5 +365,22 @@ public class SpideyBCFront {
 			msg.setMessage("Please fill out every input box");
 			msg.open();
 		}
+	}
+	
+	private static String getRunConfiguration(int index) {
+		switch(index) {
+			case 0:
+				return "debug";
+			case 1:
+				return "deploy";
+		}
+		return null;
+	}
+	
+	private static int getRunConfiguration(String runConfiguration) {
+		if(runConfiguration.equals("deploy"))
+			return 1;
+		else
+			return 0;
 	}
 }
